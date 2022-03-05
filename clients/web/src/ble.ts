07215d53bd9afe290db92e0af954742b490e41ee
rbaron <rbaron@rbaron.net>
2022-03-05 08:24:44 +0100

    Removes mentions of orientation characteristic from the web UI

--- clients/web/src/ble.ts
+++ clients/web/src/ble.ts
@@ -1,16 +1,11 @@
-import { Quaternion } from "three";
-
 const kUARTServiceUUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
 // const kUARTTXCharUUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 const kSensorServiceUUID = "9b7d5c6f-a8ca-4080-9290-d4afb5ac64a3";
 const kSensorCharacteristicUUID = "527d0f9b-db66-48c5-9089-071e1a795b6f";
-const kOrientationCharacteristicUUID = "caf0797d-71d3-4ae5-b2a6-67c18f70afa6";
 const kConfigServiceUUID = "e94989ee-7b22-4b34-b71d-a33459aea9ae";
 const kConfigCharacteristicUUID = "e65785ce-955b-42aa-95a2-b7d96806d3da";
 
 let configChar: BluetoothRemoteGATTCharacteristic | null = null;
-// let sensorChar: BluetoothRemoteGATTCharacteristic | null = null;
-// let orientationChar: BluetoothRemoteGATTCharacteristic | null = null;
 
 export type Callback = () => void;
 
@@ -40,45 +35,12 @@ export type Config = {
 
 export type onConfig = (config: Config) => void;
 
-export type Orientation = {
-  quaternion: Quaternion;
-  eulerAngles: {
-    psi: number;
-    theta: number;
-    phi: number;
-  };
-};
-
-export type onOrientation = (orientation: Orientation) => void;
-
 function onConnectRequest(
   onConnect: Callback,
   onDisconnect: Callback,
-  onOrientation: onOrientation,
   onSensor: onSensor,
   onConfig: onConfig
 ) {
-  function onOrientationEvent(event: any) {
-    const characteristic = event.target;
-    const data: DataView = characteristic.value;
-    return onOrientationData(data);
-  }
-
-  function onOrientationData(data: DataView) {
-    const w = data.getInt16(0, true) / 1000.0;
-    const x = data.getInt16(2, true) / 1000.0;
-    const y = data.getInt16(4, true) / 1000.0;
-    const z = data.getInt16(6, true) / 1000.0;
-    onOrientation({
-      quaternion: new Quaternion(x, y, z, w),
-      eulerAngles: {
-        psi: data.getInt16(8, true) / 100.0,
-        theta: data.getInt16(10, true) / 100.0,
-        phi: data.getInt16(12, true) / 100.0,
-      },
-    });
-  }
-
   function onSensorEvent(event: any) {
     const characteristic = event.target;
     const data: DataView = characteristic.value;
@@ -129,21 +91,6 @@ function onConnectRequest(
     });
   }
 
-  function handleOrientationChar(
-    characteristic: BluetoothRemoteGATTCharacteristic
-  ) {
-    // orientationChar = characteristic;
-    return Promise.all([
-      characteristic.readValue().then(onOrientationData),
-      characteristic.startNotifications().then((char) => {
-        characteristic.addEventListener(
-          "characteristicvaluechanged",
-          onOrientationEvent
-        );
-      }),
-    ]);
-  }
-
   function handleSensorChar(characteristic: BluetoothRemoteGATTCharacteristic) {
     // sensorChar = characteristic;
     return Promise.all([
@@ -159,9 +106,6 @@ function onConnectRequest(
 
   function handleSensorService(service: BluetoothRemoteGATTService) {
     return Promise.all([
-      // service
-      //   .getCharacteristic(kOrientationCharacteristicUUID)
-      //   .then(handleOrientationChar),
       service
         .getCharacteristic(kSensorCharacteristicUUID)
         .then(handleSensorChar),
@@ -206,8 +150,6 @@ function onConnectRequest(
       .then((device) => {
         device.addEventListener("gattserverdisconnected", function (ev) {
           configChar = null;
-          // sensorChar = null;
-          // orientationChar = null;
           onDisconnect();
         });
         return device.gatt!.connect();
